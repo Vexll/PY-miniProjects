94d7460bbb11a01d1e31e7d4d88d54066f947f27
Ali AlGhamdi <ali.w.alghamdi@outlook.com>
2023-12-19 08:07:49 +0300

    little updata on hangman game

--- Hangman/HangmanGame.py
+++ Hangman/HangmanGame.py
@@ -3,17 +3,10 @@ from hangman_resourse import hangman_words
 from hangman_resourse import hangman_art
 
 
-def list_formatting(word):
-    temp_list = []
-    for _ in range(len(word)):
-        temp_list.append("_")
-    return temp_list
-
-
 # random choice part
 random_choice = str(random.choice(hangman_words.word_list))
 word_length = len(random_choice)
-guessing_letters = list_formatting(random_choice)
+guessing_letters = ["_" for i in range(word_length)]
 
 # introduction part
 print("Welcome to the Hangman game")
@@ -23,7 +16,7 @@ for i in range(word_length):
     print("_", end=" ")
 print()  # new line
 
-wrong_attempts = 6  # maximum attempts is 7
+wrong_attempts = 6      # maximum attempts is 7
 while wrong_attempts >= 0:
     letter = input("give a shot: ").lower()
     # handling if the user didn't enter a letter.

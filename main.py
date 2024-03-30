46b15c4036fc7617023b4a57ec9babbe3cd9c5c1
Ali AlGhamdi <ali.w.alghamdi@outlook.com>
2024-03-30 11:12:18 +0300

    created a mini project with api

--- main.py
+++ main.py
@@ -1,5 +1,17 @@
-from typing import List
+def gcd(a, b):
+    x, y = a, b
+    while y != 0:
+        # r = x % y
+        # x = y
+        # y = r
+        x, y = y, x % y
+    return x
 
-temp = "ali"
 
-print(temp.capitalize())
+def gcd_rec(a, b):
+    if b == 0:
+        return a
+    return gcd_rec(b, a % b)
+
+
+print(gcd(287, 91))
